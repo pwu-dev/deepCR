0880528d43f4900caf89e5a12726c8897b01a253
pwu-dev <121495451+pwu-dev@users.noreply.github.com>
2025-04-10 14:52:55 +0200

    Add comments in util.py

--- deepCR/util.py
+++ deepCR/util.py
@@ -6,7 +6,7 @@ def medmask(image, mask):
     inpaint using 5*5 median filter sampling.
     :param image:
     :param mask:
-    :return:
+    :return: inpainted image
     """
     clean = np.copy(image)
     xmax = image.shape[0]
@@ -27,13 +27,30 @@ def medmask(image, mask):
 
 
 def maskMetric(PD, GT):
+    """
+    Compute metrics on detected CRs mask with ground truth mask
+    
+    Args:
+        PD : Predicted Detections
+        GT : Ground Truth mask
+        
+    Returns:
+        np.array: metrics array with TP, TN, FP and FN.
+    """
+    # If PD/GT is not a batch of images
     if len(PD.shape) == 2:
+        # Add the batch dimension
         PD = PD.reshape(1, *PD.shape)
     if len(GT.shape) == 2:
         GT = GT.reshape(1, *GT.shape)
+
+    # Initialize metrics variables
     TP, TN, FP, FN = 0, 0, 0, 0
+    
+    # Browse all images
     for i in range(GT.shape[0]):
-        P = GT[i].sum()
+        # P not used ? 
+        # P = GT[i].sum()
         TP += (PD[i][GT[i] == 1] == 1).sum()
         TN += (PD[i][GT[i] == 0] == 0).sum()
         FP += (PD[i][GT[i] == 0] == 1).sum()
